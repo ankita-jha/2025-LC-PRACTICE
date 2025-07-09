4569789b9ed10caa6e4556e34a02ebfeb4a25aae
Ankita Jha <98490276+ankita-jha@users.noreply.github.com>
2025-07-09 16:48:54 -0400

    Time: 11 ms (24.29%) | Memory: 21.2 MB (80.07%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -0,0 +1,17 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        hmap={}
+        for i in nums:
+            if i not in hmap:
+                hmap[i]=1
+            else:
+                hmap[i]+=1
+        print(hmap)
+        heap=[]
+        for key, val in hmap.items():
+                heapq.heappush(heap,(val,key))
+                if len(heap)>k:
+                    heapq.heappop(heap)
+        print(heap)
+        return [key for val, key in heap]
+        
