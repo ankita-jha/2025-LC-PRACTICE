bfcc1ef5653d270a08eb3fffea99792fff82e6ee
Ankita Jha <98490276+ankita-jha@users.noreply.github.com>
2025-07-09 17:22:39 -0400

    Time: 29 ms (5.05%) | Memory: 24.2 MB (20.73%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,14 +1,35 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # hmap={}
+        # for i in nums:
+        #     if i not in hmap:
+        #         hmap[i]=1
+        #     else:
+        #         hmap[i]+=1
+        # heap=[]
+        # for key, val in hmap.items():
+        #     heapq.heappush(heap,(val,key))
+        #     if len(heap)>k:
+        #         heapq.heappop(heap)
+        # return [key for val,key in heap]
+        '''
+        bucket sort
+        '''
         hmap={}
         for i in nums:
             if i not in hmap:
                 hmap[i]=1
             else:
                 hmap[i]+=1
-        heap=[]
+        print(hmap)
+        freq=[[] for i in range(len(nums)+1)]
         for key, val in hmap.items():
-            heapq.heappush(heap,(val,key))
-            if len(heap)>k:
-                heapq.heappop(heap)
-        return [key for val,key in heap]
+            freq[val].append(key)
+        print(freq)
+        res=[]
+        for i in range(len(freq)-1,0,-1):
+            print(freq[i])
+            for n in freq[i]:
+                res.append(n)
+                if len(res)==k:
+                    return res
