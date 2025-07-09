d8fa6cd7aadda202bea3e2a4efd34ab6da9f8bf6
Ankita Jha <98490276+ankita-jha@users.noreply.github.com>
2025-07-09 16:54:23 -0400

    Time: 3 ms (90.94%) | Memory: 21.2 MB (80.07%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -6,12 +6,9 @@ class Solution:
                 hmap[i]=1
             else:
                 hmap[i]+=1
-        print(hmap)
         heap=[]
         for key, val in hmap.items():
-                heapq.heappush(heap,(val,key))
-                if len(heap)>k:
-                    heapq.heappop(heap)
-        print(heap)
-        return [key for val, key in heap]
-        
+            heapq.heappush(heap,(val,key))
+            if len(heap)>k:
+                heapq.heappop(heap)
+        return [key for val,key in heap]
