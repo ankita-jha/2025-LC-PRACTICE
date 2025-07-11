9f956ee9dd3d03d0bc894af75cb2eac01fabd9f0
Ankita Jha <98490276+ankita-jha@users.noreply.github.com>
2025-07-11 16:03:38 -0400

    Time: 19 ms (37.05%) | Memory: 22.8 MB (13.02%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,11 +1,10 @@
-from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         hmap=defaultdict(list)
         for word in strs:
             count=[0]*26
-            for ch in word:
-                index=ord(ch)-ord('a')
+            for char in word:
+                index=ord(char)-ord('a')
                 count[index]+=1
             key=tuple(count)
             hmap[key].append(word)
